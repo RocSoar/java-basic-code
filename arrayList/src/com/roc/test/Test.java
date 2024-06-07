a5cf0b36ed594c7f0c44937a189146dd3a3545cc
RocSoar <lsyyd3454@outlook.com>
2024-06-07 22:34:49 +0800

    day17, String learning end, add some stream() code

---
+++ arrayList/src/com/roc/test/Test.java
@@ -0,0 +1,20 @@
+package com.roc.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> foo = Arrays.asList("a", "bb", "ccc", "dd", "e");
+        List<Integer> bar = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> aaa = foo.stream().map(String::length).toList();
+        var bbb = bar.stream().filter(e -> e % 2 == 0).toList();
+        int ccc = bar.stream().reduce((acc, e) -> acc + e).orElse(0);
+        String m = foo.stream().filter(e -> e.length() == 3).findFirst().orElseGet(() -> "空");
+        System.out.println(m);
+        
+    }
+}
--- mystring/src/com/roc/stringdemo/Demo1.java
+++ mystring/src/com/roc/stringdemo/Demo1.java
@@ -2,6 +2,7 @@ package com.roc.stringdemo;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Demo1 {
     public static void main(String[] args) {
@@ -18,9 +19,43 @@ public class Demo1 {
 
 //        CapitalConverter cc = new CapitalConverter();
 //        cc.run();
+        /*StringBuilder sb = new StringBuilder();
+        sb.append(1).append(2).append(3).append(4).append(5);
 
-        int[] a = new int[0];
-        System.out.println(MyUtils.arrayToString(a));
+
+        StringJoiner sj = new StringJoiner(", ", "[", "]");
+        StringJoiner sj2 = new StringJoiner(". ", "<", ">");
+        sj.add(1 + "").add(2 + "").add(3 + "").add(4 + "");
+        sj2.add("a").add("b").add("c");
+        sj.merge(sj2);
+        System.out.println(sj);*/
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("abcdefghijklmnopqrstuvwxyz");
+//        sb.append(456789);
+//        System.out.println(sb.capacity());
+//        System.out.println(sb.length());
+//        System.out.println('9' - 48);
+//        String foo = "abcdeabcde";
+//        foo.toCharArray();
+//        System.out.println(foo.contains("deabe"));
+        var foo = (char) (57);
+        System.out.println(' ' == 32);
+    }
+
+    public static String rotate(String str) {
+        return str.substring(1) + str.charAt(0);
+    }
+
+    public static String rotate2(String str) {
+        char[] chs = str.toCharArray();
+        char first = chs[0];
+        for (int i = 1; i < chs.length; i++) {
+            chs[i - 1] = chs[i];
+        }
+
+        chs[chs.length - 1] = first;
+
+        return new String(chs);
     }
 
 }
