637ed816c01937d0875fb06e6ff6c8a4a98ad2a9
rocsoar_mac <lsyyd3454@outlook.com>
2024-06-25 00:49:32 +0800

    day 31, add collection etc. learning

--- collection/src/com/roc/collection/CollectionDemo.java
+++ collection/src/com/roc/collection/CollectionDemo.java
@@ -7,22 +7,23 @@ import java.util.ListIterator;
 
 public class CollectionDemo {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
-
-        list.add("aaa");
-        list.add("bbb");
-        list.add("ccc");
-        list.add("ddd");
-
-        ListIterator<String> it = list.listIterator();
-
-        while (it.hasNext()) {
-            String s = it.next();
-            if (s.equals("bbb")) {
-                it.add("QQQ");
-            }
-            System.out.println(s);
-        }
-        System.out.println(list);
+//        List<String> list = new ArrayList<>();
+//
+//        list.add("aaa");
+//        list.add("bbb");
+//        list.add("ccc");
+//        list.add("ddd");
+//
+//        ListIterator<String> it = list.listIterator();
+//
+//        while (it.hasNext()) {
+//            String s = it.next();
+//            if (s.equals("bbb")) {
+//                it.add("QQQ");
+//            }
+//            System.out.println(s);
+//        }
+//        System.out.println(list);
+        System.out.println(32 >> 1);
     }
 }
