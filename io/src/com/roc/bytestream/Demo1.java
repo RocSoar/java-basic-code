44c5611b3e8f0453ecea54613b082b951a19d54a
RocSoar <lsyyd3454@outlook.com>
2024-07-06 00:20:32 +0800

    day45, add IO api learning

--- io/src/com/roc/bytestream/Demo1.java
+++ io/src/com/roc/bytestream/Demo1.java
@@ -1,23 +1,43 @@
 package com.roc.bytestream;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Demo1 {
     public static void main(String[] args) throws IOException {
         String s = "io";
 
-        File file = new File(s, "abc.txt");
+//        File file = new File(s, "abc.txt");
+//
+//        if (!file.exists())
+//            file.createNewFile();
+//
+//        FileOutputStream fos = new FileOutputStream("io/b.txt");
+//        fos.write(97);
+//        fos.write(98);
+//        fos.write(99);
+//        fos.close();
 
-        if (!file.exists())
-            file.createNewFile();
+        FileInputStream fis = new FileInputStream("io/break loop.png");
+        FileOutputStream fos = new FileOutputStream("io/video.mp4");
 
-        FileOutputStream fos = new FileOutputStream("io/b.txt");
-        fos.write(97);
-        fos.write(98);
-        fos.write(99);
+//        byte[] bytes = new byte[1024 * 1024 * 5];
+//        while (true) {
+//            int len = fis.read(bytes);
+//            if (len == -1)
+//                break;
+//            fos.write(bytes, 0, len);
+//        }
+//        fos.close();
+//        fis.close();
+
+        while (true) {
+            int b = fis.read();
+            if (b == -1)
+                break;
+            System.out.print((char) b + " ");
+        }
+
         fos.close();
+        fis.close();
     }
 }
