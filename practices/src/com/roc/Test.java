2bb17c5a54a5892a1bff3392391210a3dd414f96
rocsoar_mac <lsyyd3454@outlook.com>
2024-07-24 02:11:04 +0800

    二刷day1

--- practices/src/com/roc/Test.java
+++ practices/src/com/roc/Test.java
@@ -2,7 +2,9 @@ package com.roc;
 
 public class Test {
     public static void main(String[] args) {
-        int a = 0141;
-        System.out.println(a);
+        int a = 5;
+        int b = 2;
+
+        System.out.println((double) a / b);
     }
 }
