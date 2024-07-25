3178ccbc79be19ecd369bcf5580b911f86439301
RocSoar <lsyyd3454@outlook.com>
2024-07-25 21:54:04 +0800

    二刷day3

--- practices/src/com/roc/Test.java
+++ practices/src/com/roc/Test.java
@@ -1,10 +1,13 @@
 package com.roc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 2;
-
-        System.out.println((double) a / b);
+        int a = 0141;
+        System.out.println(a);
+        List<String> list = new ArrayList<>();
+        list.remove("1");
     }
 }
