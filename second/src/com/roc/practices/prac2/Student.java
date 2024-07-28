e4e78e48314d525e8a0047a57ec5b23bbcc3d2ec
RocSoar <lsyyd3454@outlook.com>
2024-07-28 22:50:38 +0800

    二刷day6

--- second/src/com/roc/practices/prac2/Student.java
+++ second/src/com/roc/practices/prac2/Student.java
@@ -1,7 +1,10 @@
 package com.roc.practices.prac2;
 
+import java.util.Objects;
+
 public class Student extends Person {
     private String gender;
+    private double height;
 
     public Student(String name, int age) {
         super(name, age);
@@ -10,5 +13,17 @@ public class Student extends Person {
 
     public void display() {
         System.out.println(getName() + getAge());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student student)) return false;
+        return Double.compare(height, student.height) == 0 && Objects.equals(gender, student.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gender, height);
     }
 }
